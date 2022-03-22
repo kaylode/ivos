017b690dd4911bb11e9595486d33f33e87c71556
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-03-23 00:59:09 +0700

    use dice and pixel acc

--- theseus/semantic/metrics/dicecoeff.py
+++ theseus/semantic/metrics/dicecoeff.py
@@ -22,13 +22,6 @@ class DiceScore(Metric):
         self.thresh = thresh
         self.num_classes = num_classes
         self.ignore_index = ignore_index
-        self.pred_type = "multi" if self.num_classes > 1 else "binary"
-
-        if self.pred_type == 'binary':
-            assert thresh is not None, "Threshold should be specified for binary segmentation"
-        if self.num_classes == 1:
-            self.num_classes+=1
-
         self.smooth = smooth
 
         self.reset()
@@ -40,26 +33,24 @@ class DiceScore(Metric):
         # outputs: (batch, num_classes, W, H)
         # targets: (batch, num_classes, W, H)
 
-        targets = batch['targets']
-        assert len(targets.shape) == 4, "Wrong shape for targets"
-        assert len(outputs.shape) == 4, "Wrong shape for targets"
-        self.sample_size += outputs.shape[0]
-        
-        if self.pred_type == 'binary':
-            predicts = (outputs > self.thresh).float()
-        elif self.pred_type =='multi':
-            predicts = torch.argmax(outputs, dim=1)
+        targets = batch['gt'].long().squeeze(0).permute(2,1,0)
+        preds = torch.from_numpy(outputs['out']).long()
 
-        predicts = predicts.detach().cpu()
         one_hot_predicts = torch.nn.functional.one_hot(
-              predicts.long(), 
+              preds.long(), 
               num_classes=self.num_classes).permute(0, 3, 1, 2)
-        
+
+        one_hot_targets = torch.nn.functional.one_hot(
+              targets.long(), 
+              num_classes=self.num_classes).permute(0, 3, 1, 2)
+
         for cl in range(self.num_classes):
             cl_pred = one_hot_predicts[:,cl,:,:]
-            cl_target = targets[:,cl,:,:]
+            cl_target = one_hot_targets[:,cl,:,:]
             score = self.binary_compute(cl_pred, cl_target)
             self.scores_list[cl] += sum(score)
+
+        self.sample_size += targets.shape[0]
         
 
     def binary_compute(self, predict: torch.Tensor, target: torch.Tensor):
@@ -78,12 +69,9 @@ class DiceScore(Metric):
 
     def value(self):
         scores_each_class = self.scores_list / self.sample_size #mean over number of samples
-        if self.pred_type == 'binary':
-            scores = scores_each_class[1] # ignore background which is label 0
+        if self.ignore_index is not None:
+            scores_each_class[self.ignore_index] = 0
+            scores = sum(scores_each_class) / (self.num_classes - 1)
         else:
-            if self.ignore_index is not None:
-                scores_each_class[self.ignore_index] = 0
-                scores = sum(scores_each_class) / (self.num_classes - 1)
-            else:
-                scores = sum(scores_each_class) / self.num_classes
+            scores = sum(scores_each_class) / self.num_classes
         return {"dice" : scores}
