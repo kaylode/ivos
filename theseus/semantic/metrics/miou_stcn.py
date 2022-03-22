f1e63aa9904a18de8df53fbbd665571c0d7625ed
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-03-22 16:43:55 +0700

    change to original stcn

---
+++ theseus/semantic/metrics/miou_stcn.py
@@ -0,0 +1,74 @@
+from typing import Any, Dict, Optional
+import torch
+from theseus.utilities.cuda import move_to
+from theseus.base.metrics.metric_template import Metric
+
+
+
+def compute_tensor_iu(seg, gt):
+    intersection = (seg & gt).float().sum()
+    union = (seg | gt).float().sum()
+
+    return intersection, union
+
+def compute_tensor_iou(seg, gt):
+    intersection, union = compute_tensor_iu(seg, gt)
+    iou = (intersection + 1e-6) / (union + 1e-6)
+    
+    return iou 
+
+class mIOU(Metric):
+    """ Mean IOU metric for ivos
+    thresh: `float`
+        threshold for binary segmentation
+    """
+    def __init__(self, 
+            eps: float = 1e-6, 
+            thresh: Optional[float] = 0.5,
+            **kwawrgs):
+
+        self.thresh = thresh
+        self.eps = eps
+
+        self.reset()
+
+    def update(self, outputs: Dict[str, Any], batch: Dict[str, Any]): 
+        """
+        Perform calculation based on prediction and targets
+        """
+        b, num_slices, _, _, _ = batch['targets'].shape
+        selector = batch.get('selector', None)
+
+        for i in range(1, num_slices):
+            iou = compute_tensor_iou(
+                outputs['mask_%d'%i]>self.thresh, batch['targets'][:,i]>self.thresh)
+            self.tar_iou += iou
+
+            if selector is not None:
+                sec_iou = compute_tensor_iou(
+                    outputs['sec_mask_%d'%i]>self.thresh, batch['sec_gt'][:,i]>self.thresh)
+                self.sec_iou += sec_iou
+
+        self.sample_size += num_slices-1 #exclude first frame
+        
+    def reset(self):
+        self.tar_iou = 0
+        self.sec_iou = 0
+        self.sample_size = 0
+
+    def value(self):
+        tar_iou_score = self.tar_iou / self.sample_size #mean over number of samples
+        sec_iou_score = self.sec_iou / self.sample_size #mean over number of samples
+        
+        tar_iou_score = move_to(tar_iou_score, torch.device('cpu'))
+        tar_iou_score = tar_iou_score.item()
+
+        if not isinstance(sec_iou_score, int):
+            sec_iou_score = move_to(sec_iou_score, torch.device('cpu'))
+            sec_iou_score = sec_iou_score.item()
+
+        return {
+            "iou1" : tar_iou_score,
+            "iou2": sec_iou_score,
+            "miou": (tar_iou_score+sec_iou_score) / 2
+        }
