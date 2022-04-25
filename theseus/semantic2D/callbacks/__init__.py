622dfa506f3949532991d7853b5c45c2000dbb3c
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-04-25 07:08:05 +0700

    add normal seg visualizer

--- theseus/semantic2D/callbacks/__init__.py
+++ theseus/semantic2D/callbacks/__init__.py
@@ -1,6 +1,7 @@
-from theseus.semantic2D.callbacks.visualize_callbacks import VisualizerCallbacks
+from theseus.semantic2D.callbacks.visualize_callbacks import VisualizerCallbacks, NormalVisualizerCallbacks
 from theseus.semantic2D.callbacks.stcn_callback import STCNCallbacks
 from theseus.base.callbacks import CALLBACKS_REGISTRY
 
 CALLBACKS_REGISTRY.register(STCNCallbacks)
 CALLBACKS_REGISTRY.register(VisualizerCallbacks)
+CALLBACKS_REGISTRY.register(NormalVisualizerCallbacks)
--- theseus/semantic2D/callbacks/visualize_callbacks.py
+++ theseus/semantic2D/callbacks/visualize_callbacks.py
@@ -8,12 +8,12 @@ from theseus.utilities.visualization.colors import color_list
 from theseus.base.callbacks.base_callbacks import Callbacks
 from theseus.utilities.loggers.observer import LoggerObserver
 from theseus.utilities.visualization.visualizer import Visualizer
-from theseus.utilities.analysis.analyzer import SemanticAnalyzer
+from theseus.semantic2D.callbacks.base_visualize_callbacks import BaseVisualizerCallbacks
 from theseus.utilities.cuda import move_to
 
 LOGGER = LoggerObserver.getLogger("main")
 
-class VisualizerCallbacks(Callbacks):
+class VisualizerCallbacks(BaseVisualizerCallbacks):
     """
     Callbacks for visualizing stuff during training
     Features:
@@ -24,135 +24,6 @@ class VisualizerCallbacks(Callbacks):
 
     def __init__(self, **kwargs) -> None:
         super().__init__()
-
-        self.visualizer = Visualizer()
-
-    def sanitycheck(self, logs: Dict=None):
-        """
-        Sanitycheck before starting. Run only when debug=True
-        """
-
-        iters = logs['iters']
-        model = self.params['trainer'].model
-        valloader = self.params['trainer'].valloader
-        trainloader = self.params['trainer'].trainloader
-        train_batch = next(iter(trainloader))
-        val_batch = next(iter(valloader))
-        trainset = trainloader.dataset
-        valset = valloader.dataset
-        self.classnames = valset.classnames
-
-        self.visualize_model(model, train_batch)
-        self.params['trainer'].evaluate_epoch()
-        self.visualize_gt(train_batch, val_batch, iters, self.classnames)
-        self.analyze_gt(trainset, valset, iters)
-
-    @torch.no_grad()
-    def visualize_model(self, model, batch):
-        # Vizualize Model Graph
-        LOGGER.text("Visualizing architecture...", level=LoggerObserver.DEBUG)
-        images = move_to(batch["inputs"], model.device)
-        LOGGER.log([{
-            'tag': "Sanitycheck/analysis/architecture",
-            'value': model.model.get_model(),
-            'type': LoggerObserver.TORCH_MODULE,
-            'kwargs': {
-                'inputs': images,
-                'log_freq': 100
-            }
-        }])
-
-    def visualize_gt(self, train_batch, val_batch, iters, classnames):
-        """
-        Visualize dataloader for sanity check 
-        """
-
-        LOGGER.text("Visualizing dataset...", level=LoggerObserver.DEBUG)
-        images = train_batch["inputs"] # (B, T, C, H, W) 
-        masks = train_batch['cls_gt'].squeeze() # (B, T, H, W) 
-
-        batch = []
-        for idx, (inputs, mask) in enumerate(zip(images, masks)): # iter through batch
-            # iter through timestamp
-            for t_input, t_mask in zip(inputs, mask):
-                img_show = self.visualizer.denormalize(t_input, mean=[0,0,0], std=[1,1,1])
-                decode_mask = self.visualizer.decode_segmap(t_mask.numpy())
-                img_show = TFF.to_tensor(img_show)
-                decode_mask = TFF.to_tensor(decode_mask/255.0)
-                img_show = torch.cat([img_show, decode_mask], dim=-1)
-                batch.append(img_show)
-        grid_img = self.visualizer.make_grid(batch)
-
-        fig = plt.figure(figsize=(16,8))
-        plt.axis('off')
-        plt.imshow(grid_img)
-
-        # segmentation color legends 
-        patches = [mpatches.Patch(color=np.array(color_list[i][::-1]), 
-                                label=classnames[i]) for i in range(len(classnames))]
-        plt.legend(handles=patches, bbox_to_anchor=(-0.03, 1), loc="upper right", borderaxespad=0., 
-                fontsize='large', ncol=(len(classnames)//10)+1)
-        plt.tight_layout(pad=0)
-
-        LOGGER.log([{
-            'tag': "Sanitycheck/train_batch",
-            'value': fig,
-            'type': LoggerObserver.FIGURE,
-            'kwargs': {
-                'step': iters
-            }
-        }])
-
-
-        images = val_batch["inputs"].squeeze().numpy() # (B, T, C, H, W) 
-        masks = val_batch['targets'].permute(3,0,1,2).long().numpy() # (B, T, H, W) 
-        guidemark = val_batch['info']['guidemark'] # (B, T, H, W) 
-
-        first = images[:guidemark, :, :, :]
-        second = images[guidemark:, :, :, :]
-        second = np.flip(second, axis=0)
-        image_show = np.concatenate([second, first[1:,:,:,:]], axis=0)
-        
-        # iter through timestamp
-        vis_inputs = []
-        for image in image_show:
-            image = image.transpose(1,2,0)
-            image = self.visualizer.denormalize(image, mean=[0,0,0], std=[1,1,1])
-            image = (image*255).astype(int)
-            vis_inputs.append(image)
-        vis_inputs = np.stack(vis_inputs, axis=0).transpose(0,3,1,2)
-
-        # iter through timestamp
-        decode_masks = []
-        decode_preds = []
-        for mask in masks:
-            decode_mask = self.visualizer.decode_segmap(mask.squeeze())
-            decode_masks.append(decode_mask)
-        decode_masks = np.stack(decode_masks, axis=0).transpose(0,3,1,2)
-        concated_vis = np.concatenate([vis_inputs, decode_masks], axis=-1)
-        
-        ###
-        LOGGER.log([{
-            'tag': "Sanitycheck/val_batch",
-            'value': concated_vis,
-            'type': LoggerObserver.VIDEO,
-            'kwargs': {
-                'step': iters,
-                'fps': 10
-            }
-        }])
-
-        plt.cla()   # Clear axis
-        plt.clf()   # Clear figure
-        plt.close()
-
-    def analyze_gt(self, trainset, valset, iters):
-        """
-        Perform simple data analysis
-        """
-
-        LOGGER.text("Analyzing datasets...", level=LoggerObserver.DEBUG)
-        return
 
     @torch.no_grad()
     def on_val_epoch_end(self, logs: Dict=None):
@@ -228,3 +99,77 @@ class VisualizerCallbacks(Callbacks):
                 'step': iters
             }
         }])
+
+class NormalVisualizerCallbacks(BaseVisualizerCallbacks):
+    """
+    Callbacks for visualizing stuff during training
+    Features:
+        - Visualize datasets; plot model architecture, analyze datasets in sanity check
+        - Visualize prediction at every end of validation
+
+    """
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__()
+
+    @torch.no_grad()
+    def on_val_epoch_end(self, logs: Dict=None):
+        """
+        After finish validation
+        """
+
+        iters = logs['iters']
+        last_batch = logs['last_batch']
+        last_outputs = logs['last_outputs']['outputs']
+        model = self.params['trainer'].model
+        valloader = self.params['trainer'].valloader
+
+        # Vizualize model predictions
+        LOGGER.text("Visualizing model predictions...", level=LoggerObserver.DEBUG)
+
+        model.eval()
+
+        images = last_batch["inputs"]
+        masks = last_batch['targets'].squeeze()
+
+        preds = torch.argmax(last_outputs, dim=1)
+        masks = torch.argmax(masks, dim=1)
+        preds = move_to(preds, torch.device('cpu'))
+
+        batch = []
+        for idx, (inputs, mask, pred) in enumerate(zip(images, masks, preds)):
+            img_show = self.visualizer.denormalize(inputs)
+            decode_mask = self.visualizer.decode_segmap(mask.numpy())
+            decode_pred = self.visualizer.decode_segmap(pred)
+            img_cam = TFF.to_tensor(img_show)
+            decode_mask = TFF.to_tensor(decode_mask/255.0)
+            decode_pred = TFF.to_tensor(decode_pred/255.0)
+            img_show = torch.cat([img_cam, decode_pred, decode_mask], dim=-1)
+            batch.append(img_show)
+        grid_img = self.visualizer.make_grid(batch)
+
+        fig = plt.figure(figsize=(16,8))
+        plt.axis('off')
+        plt.title('Raw image - Prediction - Ground Truth')
+        plt.imshow(grid_img)
+
+        # segmentation color legends 
+        classnames = valloader.dataset.classnames
+        patches = [mpatches.Patch(color=np.array(color_list[i][::-1]), 
+                                label=classnames[i]) for i in range(len(classnames))]
+        plt.legend(handles=patches, bbox_to_anchor=(-0.03, 1), loc="upper right", borderaxespad=0., 
+                fontsize='large', ncol=(len(classnames)//10)+1)
+        plt.tight_layout(pad=0)
+
+        LOGGER.log([{
+            'tag': "Validation/val_prediction",
+            'value': fig,
+            'type': LoggerObserver.FIGURE,
+            'kwargs': {
+                'step': iters
+            }
+        }])
+
+        plt.cla()   # Clear axis
+        plt.clf()   # Clear figure
+        plt.close()
