52af4dcfed1d9ddfec739c55a4642d4cba0d3a51
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-05-09 17:37:30 +0700

    new losses

--- theseus/semantic2D/losses/__init__.py
+++ theseus/semantic2D/losses/__init__.py
@@ -3,9 +3,12 @@ from theseus.base.losses import LOSS_REGISTRY
 from .ce_loss import *
 from .dice_loss import DiceLoss
 from .stcn_loss import STCNLoss
+from .stcn_loss_new import NewSTCNLoss
+
 
 LOSS_REGISTRY.register(CELoss)
 LOSS_REGISTRY.register(OhemCELoss)
 LOSS_REGISTRY.register(SmoothCELoss)
 LOSS_REGISTRY.register(DiceLoss)
 LOSS_REGISTRY.register(STCNLoss)
+LOSS_REGISTRY.register(NewSTCNLoss)
