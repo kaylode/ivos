a891c61d783d6093570b4bee3be9ed51d0c01aa3
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-05-26 14:37:26 +0700

    add lovasz + active boundary loss

--- theseus/semantic2D/losses/__init__.py
+++ theseus/semantic2D/losses/__init__.py
@@ -4,6 +4,8 @@ from .ce_loss import *
 from .tversky_loss import FocalTverskyLoss
 from .dice_loss import DiceLoss
 from .stcn_loss import STCNLoss, STCNLossV2
+from .abl import ABL
+from .lovasz_loss import LovaszSoftmax
 
 LOSS_REGISTRY.register(CELoss)
 LOSS_REGISTRY.register(OhemCELoss)
@@ -12,3 +14,5 @@ LOSS_REGISTRY.register(DiceLoss)
 LOSS_REGISTRY.register(FocalTverskyLoss)
 LOSS_REGISTRY.register(STCNLoss)
 LOSS_REGISTRY.register(STCNLossV2)
+LOSS_REGISTRY.register(ABL)
+LOSS_REGISTRY.register(LovaszSoftmax)
