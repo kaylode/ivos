cca3320a125589516d0dbf725ac8d23a08a70f62
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-06-26 05:00:03 +0000

    add transunet swinunet

--- theseus/semantic2D/models/__init__.py
+++ theseus/semantic2D/models/__init__.py
@@ -4,6 +4,11 @@ from .wrapper import ModelWithLoss
 
 from .stcn import STCNModel
 from theseus.semantic2D.models.segmodels import BaseSegModel
+from theseus.semantic2D.models.swin.swin_unet import SwinUnet
+from theseus.semantic2D.models.transunet import TransUnet
+
 
 MODEL_REGISTRY.register(STCNModel)
 MODEL_REGISTRY.register(BaseSegModel)
+MODEL_REGISTRY.register(SwinUnet)
+MODEL_REGISTRY.register(TransUnet)
--- tools/scripts/train_normal.sh
+++ tools/scripts/train_normal.sh
@@ -1,11 +1,11 @@
 python tools/train/stcn/train.py \
-      -c configs/semantic2D/flare22v2/normal/pipeline.yaml \
+      -c configs/semantic2D/flare22v2/normal/pipeline_swin.yaml \
       -o global.debug=True \
       trainer.args.print_interval=100 \
       trainer.args.evaluate_interval=1 \
       trainer.args.num_iterations=30000 \
       global.save_dir=./runs \
-      global.exp_name=dlv3pb2-flare22slicesv2-ps \
+      global.exp_name=swinunet-flare22slicesv2-ps \
       global.exist_ok=True \
       trainer.args.use_fp16=True \
       # global.resume=weights/dlv3pb2/checkpoints/last.pth
