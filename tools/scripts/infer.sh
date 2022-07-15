7249a5d4994512495110ea50d74ca76386253997
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-07-15 09:11:47 +0000

    update configs and scripts

--- tools/scripts/infer.sh
+++ tools/scripts/infer.sh
@@ -1,16 +1,17 @@
 
-# GT_DIR=$1
-ROOT_DIR=$1
-SAVE_DIR=runs/unlabelled_infer
+GT_DIR=$1
+ROOT_DIR=$2
+SAVE_DIR=runs/test_infer
 
-MODEL_NAME=cps-unetppb3-dlv3pb4_ce_lvsz_flare22slicesv2
+MODEL_NAME=cps-transunetpe-dlv3pb3_ce_lvsz_flare22slicesv2-ps2
+PROPAGATION_NAME=stcn_r50_ohmce_lvsz_aug-flare22slicesv2-ps2
 
 PRED_DIR="${SAVE_DIR}/${MODEL_NAME}/test/masks"
 OUTDIR="${SAVE_DIR}/${MODEL_NAME}/test/submission"
 WEIGHT="weights/${MODEL_NAME}/checkpoints/best.pth"
-CFG_PATH="weights/${MODEL_NAME}/checkpoints/best.pth"
 VIS_DIR="${SAVE_DIR}/${MODEL_NAME}/test/mp4"
 
+PROP_WEIGHT="weights/${PROPAGATION_NAME}/checkpoints/best.pth"
 
 # python tools/inference/infer_1stage.py \
 #               -c configs/semantic2D/flare22v2/normal/test.yaml \
@@ -20,27 +21,27 @@ VIS_DIR="${SAVE_DIR}/${MODEL_NAME}/test/mp4"
 #               global.exp_name=$MODEL_NAME \
 #               global.exist_ok=True
 
-PYTHONPATH=. python tools/inference/infer_cps.py \
-              -c configs/semantic2D/flare22v2/cps/test.yaml \
-              -o global.weights=$WEIGHT \
-              data.dataset.args.root_dir=$ROOT_DIR \
-              global.save_dir=$SAVE_DIR \
-              global.exp_name=$MODEL_NAME \
-              global.exist_ok=True
-
-# PYTHONPATH=. python tools/inference/infer_2stage_efficientv2.py \
-#               -c configs/semantic2D/flare22v2/stcn/test.yaml \
-#               -o global.ref_weights=weights/cps-unetppb3-dlv3pb2_ce_lvsz_flare22slicesv2/checkpoints/best.pth \
+# PYTHONPATH=. python tools/inference/infer_cps.py \
+#               -c configs/semantic2D/flare22v2/cps/test.yaml \
+#               -o global.weights=$WEIGHT \
 #               data.dataset.args.root_dir=$ROOT_DIR \
 #               global.save_dir=$SAVE_DIR \
 #               global.exp_name=$MODEL_NAME \
-#               global.prop_weights=$WEIGHT \
 #               global.exist_ok=True
+
+PYTHONPATH=. python tools/inference/infer_2stage_efficient.py \
+              -c configs/semantic2D/flare22v2/stcn/test.yaml \
+              -o global.ref_weights=$WEIGHT \
+              data.dataset.args.root_dir=$ROOT_DIR \
+              global.save_dir=$SAVE_DIR \
+              global.exp_name=$MODEL_NAME \
+              global.prop_weights=$PROP_WEIGHT \
+              global.exist_ok=True
 
 # PYTHONPATH=. python tools/inference/infer_refine.py \
 #               -c configs/semantic2D/flare22v2/stcn/test_refine.yaml \
 #               -o data.dataset.args.root_dir=$ROOT_DIR \
-#                 data.dataset.args.mask_dir=runs/val_infer/ensemble/cpss_flare22slicesv2/masks \
+#                 data.dataset.args.mask_dir=runs/unlabelled_infer/full/cps-transunetpe-dlv3pb3_ce_lvsz_flare22slicesv2-ps2/test/masks \
 #                 global.save_dir=$SAVE_DIR \
 #                 global.exp_name=$MODEL_NAME \
 #                 global.prop_weights=$WEIGHT \
@@ -55,10 +56,10 @@ PYTHONPATH=. python tools/inference/infer_cps.py \
 #               global.exp_name=$MODEL_NAME \
 #               global.exist_ok=True
 
-# PYTHONPATH=. python tools/postprocess/make_submission.py \
-#                 -p $PRED_DIR \
-#                 -g $GT_DIR \
-#                 -o $OUTDIR
+PYTHONPATH=. python tools/postprocess/make_submission.py \
+                -p $PRED_DIR \
+                -g $GT_DIR \
+                -o $OUTDIR
 
 # PYTHONPATH=. python tools/evaluation/DSC_NSD_eval.py \
 #                 -p $OUTDIR \
--- tools/scripts/train_cps.sh
+++ tools/scripts/train_cps.sh
@@ -3,10 +3,10 @@ python tools/train/cps/train.py \
       -o global.debug=True \
       trainer.args.print_interval=100 \
       trainer.args.evaluate_interval=1 \
-      trainer.args.num_iterations=30000 \
+      trainer.args.num_iterations=50000 \
       global.save_dir=./runs \
-      global.exp_name=cps-dlv3pb2-dlv3pb4_ce_lvsz_flare22slicesv2 \
+      global.exp_name=cps-transunetpe-dlv3pb3_ce_lvsz_flare22slicesv2-ps2 \
       global.exist_ok=True \
       trainer.args.use_fp16=True \
-      global.pretrained1=weights/dlv3pb2_ohmce_lvsz-flare22slicesv2/checkpoints/best.pth \
-      global.pretrained2=weights/dlv3pb4_ohmce_lvsz-flare22slicesv2/checkpoints/best.pth
+      global.pretrained1=weights/transunetpe_ohmce_lvsz-flare22slicesv2-ps2-pt/checkpoints/best.pth \
+      global.pretrained2=weights/dlv3pb3_ohmce_lvsz-flare22slicesv2-ps2-pt/checkpoints/best.pth \
