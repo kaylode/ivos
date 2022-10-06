7944f50e7bdfec716e2b3482af226bd8fc9c4540
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-10-06 20:09:21 +0700

    update config and scripts for stcn

--- tools/scripts/preprocess_data.sh
+++ tools/scripts/preprocess_data.sh
@@ -9,8 +9,10 @@ PROCESSED_UNLABELED_PATH="data/flare22/processed/unlabeled/images"
 
 PROCESSED_IMAGE_TRAIN_PATH="data/flare22/processed/training/TrainImage"
 PROCESSED_IMAGE_VAL_PATH="data/flare22/processed/training/ValImage"
+PROCESSED_IMAGE_VALIDATION_PATH="data/flare22/processed/validation/ValidationImage"
 PROCESSED_MASK_TRAIN_PATH="data/flare22/processed/training/TrainMask"
 PROCESSED_MASK_VAL_PATH="data/flare22/processed/training/ValMask"
+PROCESSED_MASK_VALIDATION_PATH="data/flare22/processed/validation/ValidationMask"
 
 SLICES_IMAGE_TRAIN_PATH="data/flare22/slices/training/TrainImage"
 SLICES_IMAGE_VAL_PATH="data/flare22/slices/training/ValImage"
@@ -30,61 +32,72 @@ NPY_MASK_VALIDATION_PATH="data/flare22/npy/validation/ValidationMask"
 NPY_CSV_TRAIN="data/flare22/train_npy.csv"
 NPY_CSV_VAL="data/flare22/val_npy.csv"
 
-# Process training data
-echo "Split and numpify training and validation"
-PYTHONPATH=. python tools/preprocess/split_train_val.py \
-    -i $TRAIN_PATH \
-    -o $PROCESSED_TRAIN_PATH \
-    --ratio 0.95
+# # Process training data
+# echo "Normalize training and validation"
+# PYTHONPATH=. python tools/preprocess/split_train_val.py \
+#     -i $TRAIN_PATH \
+#     -o $PROCESSED_TRAIN_PATH \
+#     --ratio 0.95
 
-#Process validation data
-echo "Normalize official validation"
-PYTHONPATH=. python tools/preprocess/process_test.py \
-    -i $VALIDATION_PATH \
-    -l $GT_VALIDATION_PATH \
-    -o $PROCESSED_VALIDATION_PATH
+# #Process validation data
+# echo "Normalize official validation"
+# PYTHONPATH=. python tools/preprocess/process_test.py \
+#     -i $VALIDATION_PATH \
+#     -l $GT_VALIDATION_PATH \
+#     -o $PROCESSED_VALIDATION_PATH \
+    # -t "Validation"
 
 # echo "Windowing CT labelled data"
-# python tools/preprocess/windowing_ct/run.py $PROCESSED_TRAIN_IMAGE_PATH $SLICES_TRAIN_IMAGE_PATH
-# python tools/preprocess/windowing_ct/run.py $PROCESSED_VAL_IMAGE_PATH $SLICES_VAL_IMAGE_PATH
-# python tools/preprocess/windowing_ct/run.py $PROCESSED_VALIDATION_PATH $SLICES_VALIDATION_IMAGE_PATH
+# python tools/preprocess/windowing_ct/run.py $PROCESSED_IMAGE_TRAIN_PATH $SLICES_IMAGE_TRAIN_PATH
+# python tools/preprocess/windowing_ct/run.py $PROCESSED_IMAGE_VAL_PATH $SLICES_IMAGE_VAL_PATH
+# python tools/preprocess/windowing_ct/run.py $PROCESSED_IMAGE_VALIDATION_PATH $SLICES_IMAGE_VALIDATION_PATH
 
-# mv $PROCESSED_TRAIN_MASK_PATH $SLICES_TRAIN_MASK_PATH
-# mv $PROCESSED_VAL_MASK_PATH $SLICES_VAL_MASK_PATH
-# mv $PROCESSED_VALIDATION_MASK_PATH $SLICES_VALIDATION_MASK_PATH
+# mv $PROCESSED_MASK_TRAIN_PATH $SLICES_MASK_TRAIN_PATH
+# mv $PROCESSED_MASK_VAL_PATH $SLICES_MASK_VAL_PATH
+# mv $PROCESSED_MASK_VALIDATION_PATH $SLICES_MASK_VALIDATION_PATH
 
 
 # echo "Numpify labelled data"
-# python tools/preprocess/windowing_ct/numpify.py $SLICES_TRAIN_IMAGE_PATH $SLICES_TRAIN_NPY_PATH
-# python tools/preprocess/windowing_ct/numpify.py $SLICES_VAL_IMAGE_PATH $SLICES_VAL_NPY_PATH
-# python tools/preprocess/windowing_ct/numpify.py $SLICES_VALIDATION_IMAGE_PATH $SLICES_VALIDATION_NPY_PATH
+# python tools/preprocess/windowing_ct/numpify.py \
+#     -i $SLICES_IMAGE_TRAIN_PATH \
+#     -g $SLICES_MASK_TRAIN_PATH \
+#     -s $NPY_IMAGE_TRAIN_PATH \
+#     -sg $NPY_MASK_TRAIN_PATH
+
+# python tools/preprocess/windowing_ct/numpify.py \
+#     -i $SLICES_IMAGE_VAL_PATH \
+#     -g $SLICES_MASK_VAL_PATH \
+#     -s $NPY_IMAGE_VAL_PATH \
+#     -sg $NPY_MASK_VAL_PATH
+
+# python tools/preprocess/windowing_ct/numpify.py \
+#     -i $SLICES_IMAGE_VALIDATION_PATH \
+#     -g $SLICES_MASK_VALIDATION_PATH \
+#     -s $NPY_IMAGE_VALIDATION_PATH \
+#     -sg $NPY_MASK_VALIDATION_PATH
 
 # echo "Make csv file"
-# python tools/preprocess/windowing_ct/make_csv.py \
-#     -i $SLICES_TRAIN_IMAGE_PATH \
-#     -g $PROCESSED_TRAIN_MASK_PATH \
-#     -o $CSV_TRAIN
-
-# python tools/preprocess/windowing_ct/make_csv.py \
-#     -i $SLICES_VAL_IMAGE_PATH \
-#     -g $PROCESSED_VAL_MASK_PATH \
-#     -o $CSV_VAL
+# python tools/preprocess/windowing_ct/make_csv.py
 
 
+#Process unlabelled data
+UNLABELLED_PATH="data/flare22/raw/unlabelled/images"
+mkdir data/flare22/processed/unlabelled
+mkdir data/flare22/npy/unlabelled
 
-# #Process unlabelled data
-# UNLABELLED_PATH="data/flare22/raw/unlabelled/images"
-# PROCESSED_UNLABELLED_PATH="data/flare22/processed/unlabelled/images"
-# SLICES_UNLABELLED_IMAGE_PATH="data/flare22/slices/unlabelled/UnlabelledImage"
-# SLICES_UNLABELLED_NPY_PATH="data/flare22/npy/unlabelled/UnlabelledImage"
+PROCESSED_UNLABELLED_PATH="data/flare22/processed/unlabelled/UnlabelledImage"
+SLICES_UNLABELLED_IMAGE_PATH="data/flare22/slices/unlabelled/UnlabelledImage"
+SLICES_UNLABELLED_NPY_PATH="data/flare22/npy/unlabelled/UnlabelledImage"
 
-# echo "Normalize unlabelled"
-# PYTHONPATH=. python tools/preprocess/process_test.py \
-#     -i $UNLABELLED_PATH \
-#     -o $PROCESSED_UNLABELLED_PATH
+echo "Normalize unlabelled"
+PYTHONPATH=. python tools/preprocess/process_test.py \
+    -i $UNLABELLED_PATH \
+    -o $PROCESSED_UNLABELLED_PATH \
+    -t "Unlabelled"
 
-# echo "Windowing CT labelled data"
-# python tools/preprocess/windowing_ct/run.py $PROCESSED_UNLABELLED_PATH $SLICES_UNLABELLED_IMAGE_PATH
+echo "Windowing CT unlabelled"
+python tools/preprocess/windowing_ct/run.py $PROCESSED_UNLABELLED_PATH $SLICES_UNLABELLED_IMAGE_PATH
 
-# echo "Numpify unlabelled data"
-# python tools/preprocess/windowing_ct/numpify.py $SLICES_UNLABELLED_IMAGE_PATH $SLICES_UNLABELLED_NPY_PATH
+python tools/preprocess/windowing_ct/numpify.py \
+    -i $SLICES_UNLABELLED_IMAGE_PATH \
+    -s $SLICES_UNLABELLED_NPY_PATH 
